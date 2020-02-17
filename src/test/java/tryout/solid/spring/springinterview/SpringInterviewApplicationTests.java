0773b09ae2c9c74ac7ad6ee4f40ae6ca7f8544cc
prgandhi <Prajit.Gandhi@aig.com>
2020-02-17 05:44:14 +0000

    Initial Commit

---
+++ src/test/java/tryout/solid/spring/springinterview/SpringInterviewApplicationTests.java
@@ -0,0 +1,16 @@
+package tryout.solid.spring.springinterview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringInterviewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
